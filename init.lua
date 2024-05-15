59c1641248b304fa744858636ec60d72585d8b63
Moisés Vilas Boas <moisesboas@gmail.com>
2024-05-14 22:01:43 -0300

    feat: configurations

--- init.lua
+++ init.lua
@@ -1,9 +1,3 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.g.mapleader = " "
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system({
@@ -15,11 +9,9 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
     lazypath,
   })
 end
+
 vim.opt.rtp:prepend(lazypath)
 
-local opt = {}
-
+require("vim-options")
 require("lazy").setup("plugins")
-
-
 
