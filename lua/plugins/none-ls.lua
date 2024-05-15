72294142cb8854568a55eaab2f6a975efcdc8723
Moisés Vilas Boas <moisesboas@gmail.com>
2024-05-15 13:49:57 -0300

    feat: auto completions

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,18 +1,18 @@
 return {
-  "nvimtools/none-ls.nvim",
-  config = function()
-    local null_ls = require("null-ls")
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
 
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.stylua, --//  for luafiles
-        null_ls.builtins.formatting.prettier, -- // for javascript / typescript
-        null_ls.builtins.formatting.eslint, --// for javascript / typescript
-        null_ls.builtins.formatting.black, --// for python
-        null_ls.builtins.formatting.gopls, --// for go lang
-      },
-    })
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua, --//  for luafiles
+				null_ls.builtins.formatting.prettier, -- // for javascript / typescript
+				null_ls.builtins.formatting.black, --// for python
+				--//			null_ls.builtins.formatting.eslint, --// for javascript / typescript
+				--//        null_ls.builtins.formatting.gopls, --// for go lang
+			},
+		})
 
-    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-  end,
+		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+	end,
 }
