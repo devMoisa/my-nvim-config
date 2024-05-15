d103f8e2146207cc44f0d6001307c7abfc7ccaaa
Moises the Bruxo <moises.miranda@mblabs.com.br>
2024-05-15 18:13:53 -0300

    fix: arch

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,40 +1,40 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",
-    "MunifTanjim/nui.nvim",
-  },
-  config = function()
-    -- Configuração do Neo-tree
-    require("neo-tree").setup({
-      window = {
-        mappings = {
-          ["l"] = "open",    -- Usar 'L' para abrir a pasta
-          ["h"] = "close_node", -- Usar 'H' para fechar a pasta
-          ["j"] = "next_sibling", -- Usar 'j' para mover para baixo
-          ["k"] = "prev_sibling", -- Usar 'k' para mover para cima
-          -- Adicionando mapeamentos para abrir splits
-          ["v"] = function(state)
-            local node = state.tree:get_node()
-            require("neo-tree.sources.filesystem.commands").open_vsplit(state, node)
-          end,
-          ["V"] = function(state)
-            local node = state.tree:get_node()
-            require("neo-tree.sources.filesystem.commands").open_split(state, node)
-          end,
-        },
-      },
-    })
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		-- Configuração do Neo-tree
+		require("neo-tree").setup({
+			window = {
+				mappings = {
+					["l"] = "open", -- Usar 'L' para abrir a pasta
+					["h"] = "close_node", -- Usar 'H' para fechar a pasta
+					["j"] = "next_sibling", -- Usar 'j' para mover para baixo
+					["k"] = "prev_sibling", -- Usar 'k' para mover para cima
+					-- Adicionando mapeamentos para abrir splits
+					["v"] = function(state)
+						local node = state.tree:get_node()
+						require("neo-tree.sources.filesystem.commands").open_vsplit(state, node)
+					end,
+					["V"] = function(state)
+						local node = state.tree:get_node()
+						require("neo-tree.sources.filesystem.commands").open_split(state, node)
+					end,
+				},
+			},
+		})
 
-    -- Mapeamento da tecla <leader>e para alternar o Neo-tree
-    vim.keymap.set("n", "<leader>e", function()
-      require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd() })
-    end, {})
+		-- Mapeamento da tecla <leader>e para alternar o Neo-tree
+		vim.keymap.set("n", "<leader>e", function()
+			require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd() })
+		end, {})
 
-    -- Mapeamento das teclas Ctrl+h e Ctrl+l para navegação entre janelas
-    vim.keymap.set("n", "<C-h>", "<C-w>h", { noremap = true, silent = true })
-    vim.keymap.set("n", "<C-l>", "<C-w>l", { noremap = true, silent = true })
-  end,
+		-- Mapeamento das teclas Ctrl+h e Ctrl+l para navegação entre janelas
+		vim.keymap.set("n", "<C-h>", "<C-w>h", { noremap = true, silent = true })
+		vim.keymap.set("n", "<C-l>", "<C-w>l", { noremap = true, silent = true })
+	end,
 }
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -5,33 +5,32 @@ vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
 
 -- Vim options
-vim.opt.number = true          -- Show line numbers
+vim.opt.number = true -- Show line numbers
 vim.opt.relativenumber = false -- Show relative line numbers
-vim.opt.cursorline = true      -- Highlight the current line
-vim.opt.wrap = false           -- Disable line wrapping
-vim.opt.smartindent = true     -- Smart indentation
-vim.opt.autoindent = true      -- Auto indentation
-vim.opt.hlsearch = true        -- Highlight search results
-vim.opt.incsearch = true       -- Incremental search
-vim.opt.ignorecase = true      -- Ignore case in search patterns
-vim.opt.smartcase = true       -- Override ignorecase if search pattern contains uppercase letters
-vim.opt.backup = false         -- Disable backup file creation
-vim.opt.writebackup = false    -- Disable backup before overwriting a file
-vim.opt.swapfile = false       -- Disable swap file creation
-vim.opt.undofile = true        -- Enable persistent undo
-vim.opt.termguicolors = true   -- Enable 24-bit RGB color in the TUI
-vim.opt.scrolloff = 8          -- Minimum number of screen lines to keep above and below the cursor
-vim.opt.sidescrolloff = 8      -- Minimum number of screen columns to keep to the left and right of the cursor
+vim.opt.cursorline = true -- Highlight the current line
+vim.opt.wrap = false -- Disable line wrapping
+vim.opt.smartindent = true -- Smart indentation
+vim.opt.autoindent = true -- Auto indentation
+vim.opt.hlsearch = true -- Highlight search results
+vim.opt.incsearch = true -- Incremental search
+vim.opt.ignorecase = true -- Ignore case in search patterns
+vim.opt.smartcase = true -- Override ignorecase if search pattern contains uppercase letters
+vim.opt.backup = false -- Disable backup file creation
+vim.opt.writebackup = false -- Disable backup before overwriting a file
+vim.opt.swapfile = false -- Disable swap file creation
+vim.opt.undofile = true -- Enable persistent undo
+vim.opt.termguicolors = true -- Enable 24-bit RGB color in the TUI
+vim.opt.scrolloff = 8 -- Minimum number of screen lines to keep above and below the cursor
+vim.opt.sidescrolloff = 8 -- Minimum number of screen columns to keep to the left and right of the cursor
 vim.opt.clipboard = "unnamedplus" -- Use system clipboard
-vim.opt.updatetime = 300       -- Faster completion
+vim.opt.updatetime = 300 -- Faster completion
 
 -- Additional configurations
-vim.opt.signcolumn = "yes"     -- Always show the sign column
-vim.opt.splitright = true      -- Vertical split to the right
-vim.opt.splitbelow = true      -- Horizontal split to the bottom
-vim.opt.showmode = false       -- We don't need to see things like -- INSERT -- anymore
+vim.opt.signcolumn = "yes" -- Always show the sign column
+vim.opt.splitright = true -- Vertical split to the right
+vim.opt.splitbelow = true -- Horizontal split to the bottom
+vim.opt.showmode = false -- We don't need to see things like -- INSERT -- anymore
 vim.opt.completeopt = { "menuone", "noselect" } -- Completion options
 
 -- Highlight on yank
 vim.cmd("au TextYankPost * silent! lua vim.highlight.on_yank()")
-
