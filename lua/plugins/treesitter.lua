6af5f66e6be55fd4e487625fdb406c8d62437887
Moises the Bruxo <moises.miranda@mblabs.com.br>
2024-05-15 18:12:13 -0300

    feat: all

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,24 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		local config = require("nvim-treesitter.configs")
-		config.setup({
-			ensure_installed = { "lua", "javascript", "typescript", "go", "html" },
-			highlight = { enable = true },
-			indent = { enable = true },
-			autotag = { enable = true },
-		})
-	end,
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      local config = require("nvim-treesitter.configs")
+      config.setup({
+        ensure_installed = { "lua", "javascript", "typescript", "go", "html" },
+        highlight = { enable = true },
+        indent = { enable = true },
+        autotag = { enable = true },
+        rainbow = {
+          enable = true,
+          extended_mode = true,
+          max_file_lines = nil,
+        },
+      })
+    end,
+  },
+  {
+    "p00f/nvim-ts-rainbow",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+  },
 }
